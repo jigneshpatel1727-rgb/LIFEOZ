05f9ddea7753713eff2320e7421912e9854bba83
jigneshpatel1727-rgb <jigneshpatel1727@gmail.com>
2026-08-08 02:24:57 +0530

    Add Expense class with properties and constructor

--- lib/models/expense.dart
+++ lib/models/expense.dart
@@ -1,1 +1,17 @@
+class Expense {
+  final String id;
+  final String title;
+  final double amount;
+  final String category;
+  final DateTime date;
+  final String note;
 
+  Expense({
+    required this.id,
+    required this.title,
+    required this.amount,
+    required this.category,
+    required this.date,
+    this.note = '',
+  });
+}
