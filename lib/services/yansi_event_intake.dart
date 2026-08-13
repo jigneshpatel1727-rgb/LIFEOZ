ee6742d94746c20373be57373d63555880809774
jigneshpatel1727-rgb <jigneshpatel1727@gmail.com>
2026-08-13 12:21:14 +0530

    Fix Yansi event intake imports

--- lib/services/yansi_event_intake.dart
+++ lib/services/yansi_event_intake.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'yansi_notification_policy.dart';
 
 /// Normalizes permitted LifeOS events before Yansi evaluates them.
