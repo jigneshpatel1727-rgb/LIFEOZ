121c079240b3a5741093892665082f1758f001da
jigneshpatel1727-rgb <jigneshpatel1727@gmail.com>
2026-08-15 08:33:07 +0530

    Use locked LIFEOZ master reference home

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-import 'lifeoz_reference_home.dart';
+import 'lifeoz_master_reference_home.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -12,12 +12,10 @@ class LifeOZApp extends StatelessWidget {
   final SharedPreferences prefs;
   const LifeOZApp({super.key, required this.prefs});
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'LIFEOZ',
-      theme: ThemeData(brightness: Brightness.dark, useMaterial3: true, scaffoldBackgroundColor: const Color(0xFF010207)),
-      home: LifeOZReferenceHome(prefs: prefs),
-    );
-  }
+  Widget build(BuildContext context) => MaterialApp(
+    debugShowCheckedModeBanner: false,
+    title: 'LIFEOZ',
+    theme: ThemeData(brightness: Brightness.dark, useMaterial3: true, scaffoldBackgroundColor: const Color(0xFF01030A)),
+    home: LifeOZMasterReferenceHome(prefs: prefs),
+  );
 }
