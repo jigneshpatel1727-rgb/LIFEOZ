b601606bb2fbbd578df0df6d3f5464fccc5ab58b
jigneshpatel1727-rgb <jigneshpatel1727@gmail.com>
2026-08-15 14:58:26 +0530

    Route app to clean finalized LifeOZ home

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-import 'lifeoz_exact_master_home.dart';
+import 'lifeoz_final_home.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -21,6 +21,6 @@ class LifeOZApp extends StatelessWidget {
       useMaterial3: true,
       scaffoldBackgroundColor: const Color(0xFF01030A),
     ),
-    home: LifeOZExactMasterHome(prefs: prefs),
+    home: LifeOZFinalHome(prefs: prefs),
   );
 }
