3e215567de2adb743532cfa6ec989e5b2e3c15e5
jigneshpatel1727-rgb <jigneshpatel1727@gmail.com>
2026-08-15 07:56:13 +0530

    Use reference-matched LIFEOZ home as app entry

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-import 'lifeoz_master_shell.dart';
+import 'lifeoz_reference_home.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -11,18 +11,13 @@ Future<void> main() async {
 class LifeOZApp extends StatelessWidget {
   final SharedPreferences prefs;
   const LifeOZApp({super.key, required this.prefs});
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'LIFEOZ',
-      theme: ThemeData(
-        brightness: Brightness.dark,
-        useMaterial3: true,
-        scaffoldBackgroundColor: const Color(0xFF010207),
-      ),
-      home: LifeOZMasterShell(prefs: prefs),
+      theme: ThemeData(brightness: Brightness.dark, useMaterial3: true, scaffoldBackgroundColor: const Color(0xFF010207)),
+      home: LifeOZReferenceHome(prefs: prefs),
     );
   }
 }
